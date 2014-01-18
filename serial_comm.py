533da5d212f759b8e89face2fe9b8c75704eaccd
Alain Hernandez <alainh@seas.upenn.edu>
2014-01-18 04:00:44 -0500

    Fixed

--- serial_comm.py
+++ serial_comm.py
@@ -5,7 +5,7 @@ import serial
 import math
 
 fs = fluidsynth.Synth()
-fs.start()
+fs.start(driver='alsa')
 
 # Load the accordion soundfont
 sfid = fs.sfload("accordion.sf2")
@@ -23,17 +23,6 @@ for i in range(256):
             sys.exit()
             pass
 
-fs.noteon(0, 60, 30)
-fs.noteon(0, 67, 30)
-fs.noteon(0, 76, 30)
-
-time.sleep(1.0)
-
-fs.noteoff(0, 60)
-fs.noteoff(0, 67)
-fs.noteoff(0, 76)
-
-time.sleep(1.0)
 
 print("Finished init\n")
 
@@ -44,6 +33,6 @@ while True:
             if x.strip() and x != '':
                 print x
                 x = int(math.fabs(float(x)))
-                a.setMul(x*x/20)
+                fs.noteon(0, 60, x);
     except serial.serialutil.SerialException:
         pass
